2a9ba81cdff3bc5558594f48209876d72c878b14
Austin Erlandson <austin@erlandson.com>
2016-01-03 00:58:47 -0600

    Zellers and dayOfTheWeek passing unit specs! woogst!

--- test/Cal.hs
+++ test/Cal.hs
@@ -1,29 +1,29 @@
 module Cal where
 
+import Data.Ratio
 
-  -----------------------------------
-  --
-  -- The following method implements Zeller's Congruence an algorithm to determin what day of the week it is.
-  --
-  --
-  -- · h is the day of the week (0 = Sunday, 1 = Monday, 2 = Tuesday, ..., 6 = Saturday)
-  --
-  -- Check out the wikipedia article for more info...
-  -- http://en.wikipedia.org/wiki/Zeller's_congruence
-  --
-  ---------------------------
+-----------------------------------
+--
+-- The following method implements Zeller's Congruence an algorithm to determin what day of the week it is.
+--
+--
+-- · h is the day of the week (0 = Sunday, 1 = Monday, 2 = Tuesday, ..., 6 = Saturday)
+--
+-- Check out the wikipedia article for more info...
+-- http://en.wikipedia.org/wiki/Zeller's_congruence
+--
+---------------------------
 
-  -- zellers day month year = (((day + floor ((26 * (month + 1)) / 10) + year + floor (year / 4) + (6 * floor (year/100)) + floor (year/400)) `mod` 7) + 6) `mod` 7
-  -- zellers :: Int -> Int -> Int -> Int
-  zellers :: (RealFrac a1, RealFrac a1, RealFrac a1) => a1 -> a1 -> a1 -> a1
-  zellers day month year = (((day + floor ((26 * (month + 1)) / 10) + year + floor (year / 4) + (6 * floor (year/100)) + floor (year/400)) `mod` 7) + 6) `mod` 7
+-- zellers day month year = (((day + floor ((26 * (month + 1)) % 10) + year + floor (year % 4) + (6 * floor (year%100)) + floor (year%400)) `mod` 7) + 6) `mod` 7
+-- zellers :: Int -> Int -> Int -> Int
+-- zellers :: a -> a -> a -> a
+zellers day month year = (((day + floor ((26 * (month + 1)) % 10) + year + floor (year % 4) + (6 * floor (year % 100)) + floor (year % 400)) `mod` 7) + 6) `mod` 7
+dayOfTheWeek day month year
+  | month <= 2 = zellers day (month + 12) (year - 1)
+  | otherwise = zellers day month year
 
-  -- dayOfTheWeek day month year
-  --   | month <= 2 = zellers day (month + 12) (year - 1)
-  --   | otherwise = zellers day month year
-
---   def zellers
---     @month += 12 and @year -= 1 if @month <= 2
---     h = (@day + ((26 * (@month + 1)) / 10).floor + @year + (@year / 4).floor + (6 * (@year/100).floor) + (@year/400).floor) % 7
---     ((h + 6) % 7)
---   end
+-- def zellers
+--   @month += 12 and @year -= 1 if @month <= 2
+--   h = (@day + ((26 * (@month + 1)) / 10).floor + @year + (@year / 4).floor + (6 * (@year/100).floor) + (@year/400).floor) % 7
+--   ((h + 6) % 7)
+-- end
--- test/CalSpec.hs
+++ test/CalSpec.hs
@@ -9,23 +9,32 @@ main = hspec spec
 spec = parallel $ do
   describe "Cal" $ do
     describe "#zellers" $ do
+      it "returns 0" $ do
+        zellers 14 1 1800 `shouldBe`  0
+      it "returns 1" $ do
+        zellers 1 10 2012 `shouldBe`  1
+      it "returns 5" $ do
+        zellers 14 3 2000 `shouldBe` 2
+      it "returns 3" $ do
+        zellers 2 6 2021 `shouldBe`  3
+      it "returns 4" $ do
+        zellers 30 4 2015 `shouldBe`  4
+      it "returns 5" $
+        zellers 1 1 2000 `shouldBe` 5
+      it "returns 6" $ do
+        zellers 28 12 2999 `shouldBe`  6
+    describe "#dayOfTheWeek" $ do
+      it "returns 0" $ do
+        dayOfTheWeek 13 2 2000 `shouldBe` 0
+      it "returns 1" $ do
+        dayOfTheWeek 1 10 2012 `shouldBe`  1
+      it "returns 2" $ do
+        dayOfTheWeek 14 1 1800 `shouldBe`  2
+      it "returns 3" $ do
+        dayOfTheWeek 2 6 2021 `shouldBe`  3
+      it "returns 4" $ do
+        dayOfTheWeek 30 4 2015 `shouldBe`  4
+      it "returns 5" $ do
+        dayOfTheWeek 27 12 2999 `shouldBe`  5
       it "returns 6" $
-        zellers 1 12 2000 `shouldBe` 6
-
---       it "returns 0" $ do
---         zellers 13 2 2000 `shouldBe` 0
-
---       it "returns one" $ do
---         zellers 1 10 2012 `shouldBe`  1
-
---       it "returns two" $ do
---         zellers 14 1 1800 `shouldBe`  2
-
---       it "returns three" $ do
---         zellers 2 6 2021 `shouldBe`  3
-
---       it "returns four" $ do
---         zellers 30 4 2015 `shouldBe`  4
-
---       it "returns five" $ do
---         zellers 27 12 2999 `shouldBe`  5
+        dayOfTheWeek 1 1 2000 `shouldBe` 6
