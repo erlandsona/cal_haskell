53a01529d663f9109bd447060f293d89b4a5ccde
Austin Erlandson <austin@erlandson.com>
2016-01-03 09:29:16 -0600

    Working on printing a month.

--- src/Month.hs
+++ src/Month.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE QuasiQuotes #-}
 module Month where
 
-import Text.RawString.QQ
 import Data.Ratio
+import Text.RawString.QQ
+import Data.Text
 
 daysInFebruary year
   | year `mod` 400 == 0 || year `mod` 4 == 0 && year `mod` 100 /= 0 = 29
@@ -15,19 +16,55 @@ numOfDays month year
   | thirtyDayMonth month = 30
   | otherwise = 31
 
-monthString month year = 
-  [r|
-   February 2015    
-Su Mo Tu We Th Fr Sa
-  |]
-  -- unlines [
-  -- "   February 2015    "
-  -- "Su Mo Tu We Th Fr Sa",
-  -- " 1  2  3  4  5  6  7",
-  -- " 8  9 10 11 12 13 14",
-  -- "15 16 17 18 19 20 21",
-  -- "22 23 24 25 26 27 28",
-  -- "                    ",
-  -- "                    "
-  -- ]
+
+columnWidth = 20
+padding = 3
+months = [undefined, "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+
+-- header :: Int -> Int -> Text
+header month year = unpack $
+  center 20 ' ' (pack $ months !! month ++ " " ++ show year)
+daysString = "Su Mo Tu We Th Fr Sa"
+-- monthNumbers = 
+--   days_of_month = String.new.rjust(first_day_of_month * PADDING)
+--   num_of_weeks = 1
+
+--   1.upto(num_of_days) do |i|
+--     date = i.to_s
+--     obj = {true => date.center(PADDING), false => date.ljust(PADDING)}
+--     if days_of_month.length > COLUMN_WIDTH
+--       str << days_of_month.rstrip + "\n"
+--       days_of_month = ""
+--       num_of_weeks += 1
+--     end
+--     days_of_month << obj[i <= 9]
+--   end
+
+--   num_of_newlines = String.new
+--   while num_of_weeks <= 6
+--     num_of_newlines += "\n"
+--     num_of_weeks += 1
+--   end
+--   str << days_of_month.rstrip + num_of_newlines
+-- end
+
+
+
+
+
+-- monthString month year = 
+-- [r|line1
+--    line2
+--    line3|]
+--   unlines [
+--     "   February 2015    ",
+--     "Su Mo Tu We Th Fr Sa",
+--     " 1  2  3  4  5  6  7",
+--     " 8  9 10 11 12 13 14",
+--     "15 16 17 18 19 20 21",
+--     "22 23 24 25 26 27 28",
+--     "                    ",
+--     "                    "
+--   ]
 
--- src/MonthSpec.hs
+++ src/MonthSpec.hs
@@ -3,6 +3,7 @@ module MonthSpec where
 
 import Test.Hspec
 import Text.RawString.QQ
+import Data.Text
 import Month
 
 main :: IO ()
@@ -20,17 +21,26 @@ spec = parallel $ describe "Month" $ do
     it "February 2016 has 29 days" $ numOfDays 2 2016 `shouldBe` 29
     it "April    2016 has 30 days" $ numOfDays 4 2016 `shouldBe` 30
     it "Other months have 31 days" $ numOfDays 5 2016 `shouldBe` 31
-  describe "#monthString" $ do
-    it "builds february 2015" $
-      monthString 2 2015
-      `shouldBe`
-      unlines [
-      "   February 2015    ",
-      "Su Mo Tu We Th Fr Sa",
-      " 1  2  3  4  5  6  7",
-      " 8  9 10 11 12 13 14",
-      "15 16 17 18 19 20 21",
-      "22 23 24 25 26 27 28",
-      "                    ",
-      "                    "
-      ]
+  describe "#header" $ do
+    it "builds 20 columns with name of Month and Year" $ do
+      header 2 2015 `shouldBe` "    February 2015   "
+  describe "#daysString" $ do
+    it "returns two letter names of days" $
+      daysString `shouldBe` "Su Mo Tu We Th Fr Sa"
+  describe "#monthNumbers" $ do
+    it "builds a string formatted with days of the week." $
+      pending
+  -- describe "#monthString" $ do
+  --   it "builds february 2015" $
+  --     monthString 2 2015
+  --     `shouldBe`
+  --     unlines [
+  --     "   February 2015    ",
+  --     "Su Mo Tu We Th Fr Sa",
+  --     " 1  2  3  4  5  6  7",
+  --     " 8  9 10 11 12 13 14",
+  --     "15 16 17 18 19 20 21",
+  --     "22 23 24 25 26 27 28",
+  --     "                    ",
+  --     "                    "
+  --     ]
