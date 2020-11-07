aa147a2cd4f1506ebc1fe4d06fc96b00c5d4e93f
Austin Erlandson <aerlandson@aspirehealthcare.com>
2020-11-07 13:27:55 -0600

    polysemy stuff...

--- src/Fundamentals.hs
+++ src/Fundamentals.hs
@@ -1,15 +1,14 @@
 module Fundamentals where
 
-data FreeFunctor f a where
-    FreeFunctor :: f a -> (a -> b) -> FreeFunctor f b
+data CoYoneda f a where
+    CoYoneda :: f a -> (a -> b) -> CoYoneda f b
 
-instance Functor (FreeFunctor f) where
+instance Functor (CoYoneda f) where
     -- fmap :: (a -> b) -> f a -> f b
-    fmap fn (FreeFunctor fA fnAToB) = FreeFunctor fA (fn . fnAToB)
+    fmap fn (CoYoneda fA fnAToB) = CoYoneda fA (fn . fnAToB)
 
-
-performTheFreeFunctorBruh :: (forall a. (a -> b) -> f a -> f b) -> FreeFunctor f b -> f b
-performTheFreeFunctorBruh fmapF (FreeFunctor fA fnAB) = fmapF fnAB fA
+performTheFreeFunctorBruh :: (forall a. (a -> b) -> f a -> f b) -> CoYoneda f b -> f b
+performTheFreeFunctorBruh fmapy (CoYoneda fOfA aToB) = fmapy aToB fOfA
 
 class Monoid' a where
     append :: a -> a -> a
@@ -22,8 +21,6 @@ instance Monoid' [a] where
 performFreeMonoidBruh :: (b -> b -> b) -> b -> [b] -> b
 performFreeMonoidBruh = foldr
 
-data MyThing a = MyThing a
-
 -- append [Sum 1, Sum 3] [Sum 2] = [Sum 1, Sum 3, Sum 2]
 
 
@@ -34,7 +31,52 @@ data MyThing a = MyThing a
 -- Instance of Monad this for Identity
 -- and a performFreeMonadBruh.
 -- Stick in Maybe and then List
-
 data Free f a
-    = Leaf a
-    | Tree (f (Free f a))
+    = Pure a -- Pure :: a -> m a
+    | Free (f (Free f a)) -- Free :: f (m a) -> m a
+
+class Monad' (m :: * -> *) where
+    pure :: a -> m a
+    bind :: (a -> m b) -> m a ->  m b
+
+-- instance Monad' (Free []) where
+--     pure = Pure
+
+--     bind (Pure ls) f = f ls
+--     bind (Free l:ls) f = flip bind f _
+
+data Huh a
+    = Some a
+    | Nada
+
+-- instance Functor Huh where
+--     -- fmap :: (a -> b) -> Huh a -> Huh b
+--     fmap f (Some a) = Some (f a)
+--     fmap _ _ = Nada
+
+instance Monad' (Free Huh) where
+    pure = Pure
+    bind f (Pure x) = f x
+    bind f (Free (Some a)) = Free $ Some ((bind f) a)
+    bind _ (Free Nada) = Free Nada
+
+
+instance Monad' (Free []) where
+    pure = Pure
+    bind f (Pure x) = f x
+    bind f (Free x) = Free $ fmap (bind f) x
+
+-- newtype May a =
+--     May { huh :: forall b. b -> (a -> b) -> b }
+
+newtype Identity a = Identity { runId :: a } deriving (Functor)
+
+instance Monad' (Free Identity) where
+    pure = Pure
+    bind f = \case
+        Pure a -> f a
+        Free a -> Free $ fmap (bind f) a
+
+
+-- runFreeIdentity :: () -> Free f a -> f a
+-- runFreeIdentity free
